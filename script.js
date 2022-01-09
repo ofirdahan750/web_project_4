da05e6ca2e6ecfec4b44adcc4c500661ceb68aed
ofirdahan750 <ofirdahan1003@gmail.com>
2022-01-09 15:26:57 +0200

    finish evething before first submit

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const elBody = document.querySelector(".page");
 
+const elRoot = document.querySelector(".root");
+
 const elProfileName = document.querySelector(".profile__name");
 const elAboutMe = document.querySelector(".profile__about-me");
 const elProfileEditBtn = document.querySelector(".profile__edit-btn");
+const elProfileAddBtn = document.querySelector(".profile__add-btn");
 
 const elPopupBox = document.querySelector(".popup-box");
-const elPopupBoxCloseBtn = document.querySelector(".popup-box__close-button");
 
 const elPlaceGridContainer = document.querySelector(".place__grid-container");
-
-const elSubmitForm = document.querySelector('form[name="popup-box_form"]');
-const elInputName = document.querySelector('input[name="name"]');
-const elInputAboutMe = document.querySelector('input[name="about_me"]');
 
 let isPopUpOpen = false;
 
@@ -67,9 +65,7 @@ const initialCards = [
 
 function onInit() {
   isPopUpOpen = false;
-  initialCards.forEach(item=>
-    item.id= makeId()
-    )
+  initialCards.forEach((item) => (item.id = makeId()));
   renderPlaceItem();
   onTogglePopUpopen(true);
 }
@@ -82,30 +78,28 @@ function onToggleLikedBtn(itemId) {
   initialCards[idx].isLiked = !initialCards[idx].isLiked;
   renderPlaceItem();
 }
-function onRemoveItem(itemId) {
+function onRemoveItem(e, itemId) {
+  e.stopPropagation();
   const idx = findIdx(itemId);
- initialCards.splice(idx,1)
+  initialCards.splice(idx, 1);
 
   renderPlaceItem();
 }
-
-
 
 function renderPlaceItem() {
   const placeItemTemplate = document.querySelector(
     "#places-item-template"
   ).content;
   const htmlStr = initialCards.map((item) => {
-    
     const placeItemElement = placeItemTemplate
       .querySelector(".places__item")
       .cloneNode(true);
-    placeItemElement.querySelector(
-      ".places__img"
-    ).style.backgroundImage = `url(${item.link})`;
-    placeItemElement.querySelector(
-      ".places__img"
-    ).alt = `a photo of ${item.name}`;
+    const elPlaceImg = placeItemElement.querySelector(".places__img");
+    elPlaceImg.style.backgroundImage = `url(${item.link})`;
+    elPlaceImg.alt = `a photo of ${item.name}`;
+    elPlaceImg.addEventListener("click", () => {
+      onTogglePopUpopen(false, "img-item", item);
+    });
     placeItemElement.querySelector(
       ".places__name"
     ).textContent = `${item.name}`;
@@ -113,47 +107,119 @@ function renderPlaceItem() {
     elBtnPlace.addEventListener("click", () => {
       onToggleLikedBtn(item.id);
     });
-    placeItemElement.querySelector('.places__remove-btn').addEventListener("click",()=> {
-      onRemoveItem(item.id)
-    })
+    placeItemElement
+      .querySelector(".places__remove-btn")
+      .addEventListener("click", (e) => {
+        onRemoveItem(e, item.id);
+      });
     item.isLiked
       ? elBtnPlace.classList.add("places__like-btn__active")
       : elBtnPlace.classList.remove("places__like-btn__active");
     return placeItemElement;
   });
-
   const elAllPlaceitem = document.querySelectorAll(".places__item");
-
   if (elAllPlaceitem.length) elPlaceGridContainer.textContent = "";
-
   elPlaceGridContainer.append(...htmlStr);
-
-  // if (elAllPlaceitem.length) Array.from(elAllPlaceitem).forEach(item =>
-  //   item.remove()
-  //   )
-  // elPlaceGridContainer.append(...htmlStr);
 }
 
-function onTogglePopUpopen(isInit) {
+function onCreateItem(elemTarget, elemType, elemClass) {
+  const newElme = document.createElement(elemType);
+  newElme.classList.add(elemClass);
+  elemTarget.append(newElme);
+  return newElme;
+}
+
+function onTogglePopUpopen(isInit, popupType = "", item = {}) {
   if (!isInit) isPopUpOpen = !isPopUpOpen;
+  const popupTemplate = document.querySelector("#popup-template").content;
+  const popupElement = popupTemplate
+    .querySelector(".popup-box__container")
+    .cloneNode(true);
+  const elPopupBoxCloseBtn = popupElement.querySelector(
+    ".popup-box__close-button"
+  );
+  const elPopupWrapper = popupElement.querySelector(".popup-box__wrapper");
+  const elPopupHeading = popupElement.querySelector(".popup-box__heading");
+  const elFirstInput = popupElement.querySelector('input[name="first-input"]');
+  const elSecInput = popupElement.querySelector('input[name="second-input"]');
+  const elPopupForm = popupElement.querySelector('form[name="popup-box_form"]');
+  const elPopupFormBtn = popupElement.querySelector(
+    ".popup-box__submit-button"
+  );
   if (!isPopUpOpen) {
     elPopupBox.classList.remove("popup-box_visible");
-    elInputName.value = "";
-    elInputAboutMe.value = "";
-    elSubmitForm.removeEventListener("submit", onHandleProfileFormSubmit);
+    if (!isInit) document.querySelector(".popup-box__container").remove();
   } else {
+    switch (popupType) {
+      case "edit_profile":
+        elPopupHeading.textContent = "Edit profile";
+        elFirstInput.value = elProfileName.textContent;
+        elSecInput.value = elAboutMe.textContent;
+        elPopupFormBtn.textContent = "Save";
+        elPopupForm.addEventListener("submit", (e) => {
+          onSubmitEditProfile(e, elFirstInput.value, elSecInput.value);
+        });
+        break;
+      case "add-item":
+        elPopupHeading.textContent = "New place";
+        elFirstInput.placeholder = "Title";
+        elSecInput.placeholder = "Image link";
+        elPopupFormBtn.textContent = "Create";
+        elPopupForm.addEventListener("submit", (e) => {
+          onSubmitAddItem(e, elFirstInput.value, elSecInput.value);
+        });
+        break;
+      case "img-item":
+        popupElement.classList.add("popup-box__container_type_img");
+        elPopupWrapper.classList.add("popup-box__wrapper_type_img");
+        elPopupWrapper.textContent = "";
+        const imgElem = onCreateItem(elPopupWrapper, "img", "popup-box__img");
+        imgElem.src = item.link;
+        const textElem = onCreateItem(
+          elPopupWrapper,
+          "p",
+          "popup-box__img-title"
+        );
+        textElem.textContent = item.name;
+        break;
+      default:
+        return;
+    }
     elPopupBox.classList.add("popup-box_visible");
-    elInputName.value = elProfileName.textContent;
-    elInputAboutMe.value = elAboutMe.textContent;
-    elSubmitForm.addEventListener("submit", onHandleProfileFormSubmit);
+    elPopupBox.addEventListener("mousedown", (e) => {
+      e.stopPropagation();
+      console.log("e.which:", e.which);
+      if (
+        e.target.classList.contains(`popup-box` && "popup-box_visible") &&
+        e.which !== 3
+      ) {
+        onTogglePopUpopen();
+      }
+    });
+    elPopupBoxCloseBtn.addEventListener("click", () => {
+      onTogglePopUpopen();
+    });
+    elPopupBox.append(popupElement);
   }
 }
 
-function onHandleProfileFormSubmit(evt) {
+function onSubmitEditProfile(evt, profileNameVal, profileAboutMeVal) {
   evt.preventDefault();
-  elProfileName.textContent = elInputName.value;
-  elAboutMe.textContent = elInputAboutMe.value;
+  elProfileName.textContent = profileNameVal;
+  elAboutMe.textContent = profileAboutMeVal;
   onTogglePopUpopen();
+}
+function onSubmitAddItem(evt, itemName, itemSrc) {
+  evt.preventDefault();
+  const newItem = {
+    id: makeId(),
+    name: itemName,
+    link: itemSrc,
+    isLike: false,
+  };
+  initialCards.push(newItem);
+  onTogglePopUpopen();
+  renderPlaceItem();
 }
 
 elBody.addEventListener = addEventListener("load", () => {
@@ -161,11 +227,8 @@ elBody.addEventListener = addEventListener("load", () => {
 });
 
 elProfileEditBtn.addEventListener("click", () => {
-  onTogglePopUpopen();
+  onTogglePopUpopen(false, "edit_profile");
 });
-
-elPopupBoxCloseBtn.addEventListener("click", () => {
-  onTogglePopUpopen();
+elProfileAddBtn.addEventListener("click", () => {
+  onTogglePopUpopen(false, "add-item");
 });
-
-// elSubmitForm.addEventListener("submit", onHandleProfileFormSubmit);
