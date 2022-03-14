8b6e51694cbcf8e20c77e416e6d9c10ff41f33b1
ofirdahan750 <ofirdahan1003@gmail.com>
2022-03-14 12:56:35 +0200

    test deply

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,6 +23,11 @@ const initialCards = [
     name: "Lago di Braies",
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
+  {
+    name: "Beersheba Turkish railway station",
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/%D7%9E%D7%AA%D7%97%D7%9D_%D7%AA%D7%97%D7%A0%D7%AA_%D7%94%D7%A8%D7%9B%D7%91%D7%AA_%D7%94%D7%98%D7%95%D7%A8%D7%A7%D7%99%D7%AA_%D7%91%D7%91%D7%90%D7%A8_%D7%A9%D7%91%D7%A2.jpg/1280px-%D7%9E%D7%AA%D7%97%D7%9D_%D7%AA%D7%97%D7%A0%D7%AA_%D7%94%D7%A8%D7%9B%D7%91%D7%AA_%D7%94%D7%98%D7%95%D7%A8%D7%A7%D7%99%D7%AA_%D7%91%D7%91%D7%90%D7%A8_%D7%A9%D7%91%D7%A2.jpg",
+  },
+  
 ];
 const formValidators = {};
 
