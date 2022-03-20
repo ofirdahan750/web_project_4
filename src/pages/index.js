5f81703f790d8864dc9847126bb589a1d2357d06
ofirdahan750 <ofirdahan1003@gmail.com>
2022-03-20 17:40:43 +0200

    test confing gitigorne

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import UserInfo from "../components/UserInfo.js";
+import {API_KEY} from "../../config.js";
+console.log('confing:',  API_KEY)
 import {
   aboutMeInput,
   nameInput,
