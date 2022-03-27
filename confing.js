ccc721e4067fb625cb8236a3692c7f10168296db
Ofir Dahan <77748302+ofirdahan750@users.noreply.github.com>
2022-03-27 13:22:49 +0300

    Delete confing.js

--- confing.js
+++
@@ -1,8 +0,0 @@
-const apiConfing = {
-  baseUrl: "https://around.nomoreparties.co/v1/group-12/",
-  headers: {
-    authorization: "035a3e80-0d3f-421b-b4fa-7f406e49ad5f",
-    "Content-Type": "application/json",
-  },
-};
-export { apiConfing };
